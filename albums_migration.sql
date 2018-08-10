f58ebd76c7a1667c0efa7b2f36bba8afa10f819d
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-08-10 14:50:03 -0500

    sql prep work

---
+++ albums_migration.sql
@@ -0,0 +1,18 @@
+SHOW DATABASES;
+
+USE codeup_test_db;
+
+DROP TABLE IF EXISTS albums;
+
+CREATE TABLE albums(
+
+    id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+    artist VARCHAR (100),
+    name VARCHAR (100),
+    release_date INT,
+    sales FLOAT,
+    genre VARCHAR (50),
+    PRIMARY KEY (id)
+);
+
+SHOW TABLES;
