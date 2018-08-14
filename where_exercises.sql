d6d2240f2292b98cfed009faef3a93a0755dd415
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-08-14 11:08:27 -0500

    limit in sql

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,20 +1,20 @@
 USE employees;
 
-SELECT * FROM employees.employees
+SELECT * FROM employees
 WHERE gender = 'M'
 AND (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya'));
 
-SELECT * FROM employees.employees
+SELECT * FROM employees
 WHERE last_name LIKE 'E%'
 AND last_name LIKE '%E';
 
 
-SELECT * FROM employees.employees
+SELECT * FROM employees
 WHERE birth_date LIKE '%-12-25' AND (hire_date LIKE '199%');
 
-SELECT * FROM employees.employees
+SELECT * FROM employees
 WHERE birth_date LIKE '%-12-25'
 
-SELECT * FROM employees.employees
+SELECT * FROM employees
 WHERE last_name LIKE '%q%'  AND last_name NOT LIKE '%qu%';
 
