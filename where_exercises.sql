8edf1f13b102c11dc8f55caf340847df1ef6840d
Collier Bluff <collierbluff@Colliers-MacBook-Air.local>
2018-08-14 09:22:20 -0500

    mild refactor

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,14 +1,20 @@
 USE employees;
 
-SELECT * FROM employees.employees WHERE gender = 'M' AND (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya'));
+SELECT * FROM employees.employees
+WHERE gender = 'M'
+AND (first_name ='Irena' OR first_name = 'Vidya' OR first_name = 'Maya'));
 
-SELECT * FROM employees.employees WHERE last_name LIKE 'E%' AND last_name LIKE '%E';
+SELECT * FROM employees.employees
+WHERE last_name LIKE 'E%'
+AND last_name LIKE '%E';
 
 
---consider refactor
-SELECT * FROM employees.employees WHERE birth_date LIKE '%-12-25' AND (hire_date BETWEEN '1990-01-01' AND '1999-12-31');
+SELECT * FROM employees.employees
+WHERE birth_date LIKE '%-12-25' AND (hire_date LIKE '199%');
 
-SELECT * FROM employees.employees WHERE birth_date LIKE '%-12-25'
+SELECT * FROM employees.employees
+WHERE birth_date LIKE '%-12-25'
 
-SELECT * FROM employees.employees WHERE last_name LIKE '%q%'  AND last_name NOT LIKE '%qu%';
+SELECT * FROM employees.employees
+WHERE last_name LIKE '%q%'  AND last_name NOT LIKE '%qu%';
 
